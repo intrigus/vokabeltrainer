fb135818f77c4f90f0713a009c13112e4b732b44
intrigus <abc123zeus@live.de>
2015-02-26 20:03:46 +0100

    ....

---
+++ vokabeltrainer/src/de/igames/vokabeltrainer/StartClass.java
@@ -0,0 +1,10 @@
+package de.igames.vokabeltrainer;
+
+public class StartClass {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("test)");
+    }
+
+}
